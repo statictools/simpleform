97eb6a07d46d4e3d3f17484d9592c4dc75234d11
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-01-27 15:17:59 +0530

    move code related to the message to the message module

--- web/controllers/external/message_controller.ex
+++ web/controllers/external/message_controller.ex
@@ -11,35 +11,23 @@ defmodule Simpleform.External.MessageController do
   @filter_params ["id", "_utf8"]
 
   def create(conn, form_params = %{ "id" => form_id }) do
-    form_params = form_params |> Map.drop(@filter_params)
+
+    message_params = form_params |> Map.drop(@filter_params)
 
     # find the form
-    # TODO: do we need to find the form?
-    # we could probably background this stuff
     case Repo.get_by(Form, public_id: form_id) do
       nil ->
         Logger.debug "Did not find the form"
-        redirect(conn, to: "/")
+        # TODO: create a help page for users to find and debug
+        # their issue, lets not silently fail the user
+        redirect(conn, to: "/error/form-notfound")
       form ->
-        # create the message in the database
-        {_, json} = Poison.encode(form_params)
-        Repo.insert(%Message{form_id: form_id, data: json})
-        #Repo.insert(Message, %{form_id: form.id, data: json})
-
-        # send a notification to the user
-        Logger.debug "FOUND: #{form.name}"
-        redirect(conn, to: "/")
+        case Message.save(form, message_params) do
+          {:ok, redirect_url} -> redirect(conn, to: redirect_url)
+          error_code -> redirect(conn, to: "/error/#{error_code}") #TODO: show the user a helpful message
+        end
     end
 
   end
 
 end
-
-#case Repo.insert(changeset) do
- #{:ok, _form} ->
-   #conn
-   #|> put_flash(:info, "Form created successfully.")
-   #|> redirect(to: form_path(conn, :index))
- #{:error, changeset} ->
-   #render(conn, "new.html", changeset: changeset)
-   #end
--- web/models/message.ex
+++ web/models/message.ex
@@ -1,10 +1,27 @@
 defmodule Simpleform.Message do
   use Simpleform.Web, :model
 
+  alias Simpleform.Repo
+  alias Simpleform.Message
+
   schema "messages" do
     field :data, :string
     field :form_id, Ecto.UUID
 
     timestamps
+  end
+
+  @doc """
+  Saves a message associated with the input form and the input data
+  and returns the redirect url which can then be used to redirect the user
+  """
+  def save(form, message_params) do
+    # we could probably background this stuff
+    # create the message in the database
+    # TODO: handle json errors
+    {_, json} = Poison.encode(message_params)
+    Repo.insert(%Message{form_id: form.public_id, data: json})
+    #TODO: redirect the user to the redirect path
+    {:ok, "/"}
   end
 end
