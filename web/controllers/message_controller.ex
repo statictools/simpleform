07f6b72ba1b4efd1ee4c9e3c4ff69d6959ece011
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-05-20 14:54:11 +0530

    fixed bug in form

--- web/controllers/message_controller.ex
+++ web/controllers/message_controller.ex
@@ -8,4 +8,9 @@ defmodule Simpleform.MessageController do
     message = Repo.get_by Message, id: id
     render(conn, "show.html", message: message)
   end
+
+  def index(conn, %{"form_id" => form_id}) do
+    messages = Repo.all(from m in Message, select: m, where: m.form_id == ^form_id)
+    json conn, messages
+  end
 end
